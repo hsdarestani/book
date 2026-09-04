514d4322d3d8083cf174deb6763eab4b40522df9
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-04 19:27:59 +0330

    Register referral delivery model

--- booking/apps.py
+++ booking/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
+
 class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'
     verbose_name = 'Buchung'
+
+    def ready(self):
+        from . import referral_models  # noqa: F401
